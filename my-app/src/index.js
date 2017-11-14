aa204d96d5482a2c7e130a200f55bec5d4074a58
leighwhite96 <leigh_white@live.co.uk>
2017-11-14 11:13:38 +0000

    material ui

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Page = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+ReactDOM.render(<Page />, document.getElementById('root'));
 registerServiceWorker();
