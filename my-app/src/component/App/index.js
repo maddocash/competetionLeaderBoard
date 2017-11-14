dfc76b18ce1f5bf506c399d24df97f8b4b6ac9c8
maddocash <danksashleigh@gmail.com>
2017-11-14 11:47:46 +0000

    tournamentRow

--- my-app/src/component/App/index.js
+++ my-app/src/component/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from '../Form';
+import TournamentRow from '../TournamentRow';
 
 class App extends Component {
   constructor(props){
@@ -13,7 +14,7 @@ class App extends Component {
     this.setState(prevState => ({tournament: data}))
   }
   render() {
-    return ( 
+    return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
@@ -22,6 +23,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Form onSubmit={this._onSubmit} />
+        <TournamentRow row={[1,2,3,4,5]} />
       </div>
     );
   }
