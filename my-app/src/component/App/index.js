1908d3495b956468f3c069d7042bbb5afd173b70
Matthew England <MjE360@hotmail.co.uk>
2017-11-14 11:54:00 +0000

    Match & Branch created

--- my-app/src/component/App/index.js
+++ my-app/src/component/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from '../Form';
+import Match from '../Match';
 
 class App extends Component {
   constructor(props){
@@ -13,14 +14,8 @@ class App extends Component {
     this.setState(prevState => ({tournament: data}))
   }
   render() {
-    return ( 
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return (
+      <div>
         <Form onSubmit={this._onSubmit} />
       </div>
     );
