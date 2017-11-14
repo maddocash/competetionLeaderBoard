deefb315afd5cf0ee46645a011585f91271a82a7
Maddocash <31258232+maddocash@users.noreply.github.com>
2017-11-14 12:20:07 +0000

    Update index.js

--- my-app/src/component/App/index.js
+++ my-app/src/component/App/index.js
@@ -22,16 +22,6 @@ class App extends Component {
   render() {
     return (
 
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Form onSubmit={this._onSubmit} />
-        <Player />
-
       <div>
         <Form onSubmit={this._onSubmit} />
         <TournamentRow row={[1,2,3,4,5]} />
